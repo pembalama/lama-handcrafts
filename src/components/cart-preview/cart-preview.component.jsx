64514598f2360864af17f85ebe28121823229080
pembalama <pemba.t.lama@gmail.com>
2020-08-18 08:09:04 -0500

    completed cart preview and added styles

--- src/components/cart-preview/cart-preview.component.jsx
+++ src/components/cart-preview/cart-preview.component.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-preview.styles.scss';
 
-const CartPreview = () => {
+const CartPreview = ({ cartItems }) => {
 	return (
 		<div className="cart-dropdown">
-			<div className="cart-items" />
+			<div className="cart-items">
+				{cartItems.map(cartItem => {
+					return <CartItem key={cartItem.id} item={cartItem} />;
+				})}
+			</div>
 			<CustomButton>Checkout</CustomButton>
 		</div>
 	);
 };
 
-export default CartPreview;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	cartItems,
+});
+
+export default connect(mapStateToProps)(CartPreview);
