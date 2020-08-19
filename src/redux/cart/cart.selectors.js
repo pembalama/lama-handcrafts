7ad0af5361be3b396aa3ca9fb2d69a9ab66a8479
pembalama <pemba.t.lama@gmail.com>
2020-08-19 11:01:21 -0500

    added reselect library to use memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+const selectCart = state => {
+	return state.cart;
+};
+
+export const selectCartItems = createSelector(
+	[selectCart],
+	cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+	[selectCartItems],
+	cartItems => {
+		return cartItems.reduce((acc, cartItem) => {
+			return acc + cartItem.quantity;
+		}, 0);
+	}
+);
