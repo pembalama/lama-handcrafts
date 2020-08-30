693fcc1877b90234c929156ee8d78636bfca3a8c
pembalama <pemba.t.lama@gmail.com>
2020-08-30 13:17:52 -0500

    updated directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import { Link } from 'react-router-dom';
 
 import MenuItem from '../menu-item/menu-item.component';
 
@@ -9,11 +10,31 @@ import './directory.styles.scss';
 
 const Directory = ({ sections }) => {
 	return (
-		<div className="directory-menu">
-			{sections.map(({ id, ...otherSectionProps }) => {
-				return <MenuItem key={id} {...otherSectionProps} />;
-			})}
-		</div>
+		<>
+			<header class="hero">
+				<div class="container spacing">
+					<h1 class="primary-title">Feel the comfortness</h1>
+					{/* <p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
+						perspiciatis facilis beatae laudantium quidem enim sit sequi!
+					</p> */}
+					<Link to="/shop" class="btn">
+						Shop Now
+					</Link>
+				</div>
+			</header>
+			<div class="shop-category">
+				<div class="container">
+					<h2 class="section-title">Shop by Category</h2>
+
+					<div className="directory-menu">
+						{sections.map(({ id, ...otherSectionProps }) => {
+							return <MenuItem key={id} {...otherSectionProps} />;
+						})}
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -15,7 +15,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
 			}}
 		/>
 		<div className="content">
-			{/* <h1 className="title">{title.toUpperCase()}</h1> */}
 			<span className="subtitle">SHOP {title.toUpperCase()}</span>
 		</div>
 	</div>
